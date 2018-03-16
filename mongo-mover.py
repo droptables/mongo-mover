afc528d66e192b93fa922c1f132442e20cbc8f82
noah corradin <noah.corradin@autodesk.com>
2018-03-15 21:30:50 -0700

    using mongodump instead

--- mongo-mover.py
+++ mongo-mover.py
@@ -1,55 +1,7 @@
-import pymongo, subprocess, argparse, sys, os
+import pymongo, subprocess, argparse, sys, os, datetime, shutil, zipfile
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC
 from Crypto.Protocol.KDF import PBKDF2
-
-
-def export_data(args):
-	command="mongoexport -h "+args.server+" --db "+args.database+" --collection "+args.collection+" --out "+args.destination
-	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-
-def move_exported_data():
-	pass
-
-def encrypt(args):
-	print 'Encrypting output file...'
-	try:
-	    inputfile = open(args.destination, "rb")
-	except IOError:
-	    sys.exit("Could not open the exported destination file "+str(args.destination))
-
-	# output file
-	try:
-	    output = open(args.destination+".encrypted", "wb")
-	except IOError:
-	    sys.exit("Could not create the encrypted output file "+str(args.destination+".encrypted"))
-
-	# make 256bits keys for encryption and mac
-	salt = args.salt
-	kdf = PBKDF2(args.key, salt, 64, 1000)
-	key = kdf[:32]
-	key_mac = kdf[32:]
-
-	# create HMAC
-	mac = HMAC.new(key_mac) # default is MD5
-
-	# encryption
-	iv = os.urandom(16)
-	cipher = AES.new(key, AES.MODE_CFB, iv)    
-
-	encrypted = cipher.encrypt(inputfile.read())
-	mac.update(iv + encrypted)
-
-	# output
-	output.write(mac.hexdigest())
-	output.write(iv)
-	output.write(encrypted)
-
-	#delete data from memory
-	del salt
-	del args
-	print 'Done!'
 
 
 def decrypt(args):
@@ -96,12 +48,76 @@ def decrypt(args):
 	del args	
 	print 'Done!'
 
+def dump_database(args):
+
+	if os.path.isfile(args.destination):
+		os.remove(args.destination)
+
+	if os.path.isdir("dump"):
+		shutil.rmtree("dump")
+
+	command="mongodump -h "+args.server+" -d "+args.database
+	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+
+	zipf = zipfile.ZipFile(args.destination, 'w', zipfile.ZIP_DEFLATED)
+
+	for root, dirs, files in os.walk("dump"):
+		for file in files:
+		    zipf.write(encrypt_file(os.path.join(root, file),args))
+		    os.remove(os.path.join(root, file))
+		    print 'Done!'
+
+	zipf.close()
+
+	if os.path.isdir("dump"):
+		shutil.rmtree("dump")	
+
+
+def encrypt_file(file, args):
+	print 'Encrypting and writiing to zip file: '+str(file)+"..."
+	# input file
+	try:
+	    inputfile = open(file, "rb")
+	except IOError:
+	    sys.exit("Could not open the exported destination file "+file)
+
+	# output file
+	try:
+	    output = open(file+".encrypted", "wb")
+	except IOError:
+	    sys.exit("Could not create the encrypted output file "+file+".encrypted")
+
+	# make 256bits keys for encryption and mac
+	salt = args.salt
+	kdf = PBKDF2(args.key, salt, 64, 1000)
+	key = kdf[:32]
+	key_mac = kdf[32:]
+
+	# create HMAC
+	mac = HMAC.new(key_mac) # default is MD5
+
+	# encryption
+	iv = os.urandom(16)
+	cipher = AES.new(key, AES.MODE_CFB, iv)    
+
+	encrypted = cipher.encrypt(inputfile.read())
+	mac.update(iv + encrypted)
+
+	# output
+	output.write(mac.hexdigest())
+	output.write(iv)
+	output.write(encrypted)
+
+	#delete data from memory
+	del salt
+	del args
+	return file+".encrypted"	
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='MongoDB backup script with salted AES encryption/decryption.')
 	parser.add_argument('-s','--server', action='store', dest="server", help="Server IP/DNS of MongoDB Server.")
 	parser.add_argument('-db','--database', action='store', dest="database", help="Database within MongoDB to be backed up.")
-	parser.add_argument('-c','--collection', action='store', dest="collection", help="Collection within MongoDB database to be backed up.")
 	parser.add_argument('-ed','--export-destination', action='store', dest="destination", help="Path of the destination for the exported data.")
 	parser.add_argument('-k','--key', action='store', dest="key", help="Key used to encrypt/decrypt data export.")
 	parser.add_argument('-salt','--salt', action='store', dest="salt", help="Salt used to encrypt/decrypt data export.")	
@@ -114,10 +130,11 @@ if __name__ == '__main__':
 		decrypt(args)
 		sys.exit()
 
-	export_data(args)
+	#export_data(args)
+	dump_database(args)
 
-	if args.key:
-		if not args.salt:
-			print 'Need a salt with --salt...'
-			sys.exit()		
-		encrypt(args)
+	# if args.key:
+	# 	if not args.salt:
+	# 		print 'Need a salt with --salt...'
+	# 		sys.exit()		
+	# 	encrypt(args)
